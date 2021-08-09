a536b32c4306fe2bf468cca7eb12b218c6f9438c
bwilliford <bwilliford@gmail.com>
2021-08-09 09:43:52 -0500

    Chalkboard revamp

--- js/chalkboard.js
+++ js/chalkboard.js
@@ -13,6 +13,11 @@ let strokeColor = 'white';
 let strokeSize = 2;
 let chalkActivated = 0;
 
+let initialX;
+let initialY;
+let finalX;
+let finalY;
+
 //Initialization of PaperJS and attaching to canvas
 paper.install(window);
 //window.onload = function() {
@@ -38,6 +43,79 @@ function initializePaper() {
         if (strokeColor === 'white' || strokeColor === 'black') {
             sketch.push(path);
         }
+    }
+
+    line = new Tool();
+    line.onMouseDown = function(event) {
+        path = new paper.Path();
+        path.strokeColor = strokeColor;
+        path.strokeWidth = strokeSize;
+        path.add(event.point);
+        path.add(event.point);
+    }
+    line.onMouseDrag = function(event) {
+        path.segments[1].point = event.point;
+    }
+
+    circle = new Tool();
+    circle.onMouseDown = function(event) {
+        path = new paper.Path();
+        initialX = event.point.x;
+        initialY = event.point.y;
+    }
+    circle.onMouseDrag = function(event) {
+        path.remove();
+        finalX = event.point.x;
+        finalY = event.point.y;
+        rect =  new paper.Rectangle(new Point(initialX, initialY), new Point(finalX, finalY));
+        path = new paper.Path.Ellipse(rect);
+        path.strokeColor = strokeColor;
+        path.strokeWidth = strokeSize;
+    }
+    circle.onMouseUp = function(event) {
+        finalX = event.point.x;
+        finalY = event.point.y;
+        rect =  new paper.Rectangle(new Point(initialX, initialY), new Point(finalX, finalY));
+        path = new paper.Path.Ellipse(rect);
+        path.strokeColor = strokeColor;
+        path.strokeWidth = strokeSize;
+    }
+
+    square = new Tool();
+    square.onMouseDown = function(event) {
+        path = new paper.Path();
+        initialX = event.point.x;
+        initialY = event.point.y;
+    }
+    square.onMouseDrag = function(event) {
+        path.remove();
+        finalX = event.point.x;
+        finalY = event.point.y;
+        path =  new paper.Path.Rectangle(new Point(initialX, initialY), new Point(finalX, finalY));
+        path.strokeColor = strokeColor;
+        path.strokeWidth = strokeSize;
+    }
+    square.onMouseUp = function(event) {
+        finalX = event.point.x;
+        finalY = event.point.y;
+        path = new paper.Path.Rectangle(new Point(initialX, initialY), new Point(finalX, finalY));
+        path.strokeColor = strokeColor;
+        path.strokeWidth = strokeSize;
+    }
+
+    //Text tool
+    textInput = new Tool();
+    textInput.onMouseUp = function(event) {
+        let x = event.point.x - 20;
+        let y = event.point.y - 20;
+        let input = document.createElement("input");
+        input.type = "text";
+        input.className = "chalkboardText";
+        input.style.top = y+"px";
+        input.style.left = x+"px";
+        input.style.color = strokeColor;
+        document.getElementById('textContainer').appendChild(input);
+        input.click();
     }
 
     //Eraser tool
@@ -68,6 +146,12 @@ function initializePaper() {
 
 //Main functions
 
+//Clear canvas 
+function clearChalkboard() {
+    paper.project.activeLayer.removeChildren();
+    document.getElementById('textContainer').innerHTML = "";
+}
+
 //Toggle background from chalkboard to whiteboard and vice-versa
 function toggleBackground() {
     if (chalkboard.className === "chalkboard") {
@@ -93,19 +177,52 @@ function toggleStrokeColors(color) {
     }
 }
 
+//Change tools
+function changeTool(tool) {
+    let tools = ['draw','line','square','circle','text','eraser'];
+    for (let i = 0; i < tools.length; i++) {
+        document.getElementById(tools[i]).className = "";
+    }
+
+    if (tool === 'text') {
+        textInput.activate();
+        document.getElementById('chalkboard').style.cursor = "url('./images/icons/text.svg') 0 32, pointer";
+    }
+    else if (tool === 'draw') {
+        chalk.activate();
+        document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursor.svg') 0 32, pointer";
+    }
+    else if (tool === 'line') {
+        line.activate();
+        document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursor.svg') 0 32, pointer";
+    }
+    else if (tool === 'square') {
+        square.activate();
+        document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursor.svg') 0 32, pointer";
+    }
+    else if (tool === 'circle') {
+        circle.activate();
+        document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursor.svg') 0 32, pointer";
+    }
+    else if (tool === 'eraser') {
+        eraser.activate();
+        document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursorEraser.svg') 0 32, pointer";
+    }
+
+    document.getElementById(tool).className = "active";
+}
+
 //Change colors
 function changeColor(color) {
     //Reset active classes
-    for (let i = 0; i < toolbar.children.length; i++) {
-        toolbar.children[i].className = "";
+    let colors = ['mainColor','red','blue','green','purple','orange'];
+    for (let i = 0; i < colors.length; i++) {
+        document.getElementById(colors[i]).className = "";
     }
-
-    //Activate drawing tool
-    chalk.activate();
 
     //Change color
     document.getElementById(color).className = "active";
-    if (color === 'mainColor') {
+    if (color === 'white') {
         if (chalkboard.classList.contains('chalkboard')) {
             strokeColor = 'white';
             document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursor.svg') 0 32, pointer";
@@ -135,16 +252,6 @@ function changeColor(color) {
         strokeColor = '#d69e54'; 
         document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursorOrange.svg') 0 32, pointer";
     }
-}
-
-function toggleEraser() {
-    //Reset active classes
-    for (let i = 0; i < toolbar.children.length; i++) {
-        toolbar.children[i].className = "";
-    }
-    document.getElementById('eraser').className = "active";
-    document.getElementById('chalkboard').style.cursor = "url('./images/icons/sketchCursorEraser.svg') 0 32, pointer";
-    eraser.activate();
 }
 
 function changeTemplate() {
